84a8b99c5ca555f1f620b94e85e2bea98d596bd4
JackPeacock <birdie.jack@gmail.com>
2014-07-22 23:57:39 +0100

    hopefully removing all duplicates

--- bedhistogram.R
+++
@@ -1,1 +0,0 @@
-histogram(MVGdf$bed[MVGdf$bed %in% names(table(MVGdf$bed)[table(MVGdf$bed)>4])]
--- legAverages.R
+++
@@ -1,9 +0,0 @@
-legAverage <- vector()
-
-for (n in 1:max(df$leg)) {
-        legdf <- df[df$leg==n,]
-        total = sum(legdf$dartScore)
-        legAverage = c(legAverage, total*3/nrow(legdf))
-}
-
-plot(1:max(df$leg), legAverage)
